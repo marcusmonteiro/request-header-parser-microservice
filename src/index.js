5d57798c71b400aa47491d8d08e158b6b04d88fa
Marcus Monteiro <mvsouza007@gmail.com>
2016-11-14 17:21:15 -0200

    Finish first version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import express from 'express'
+import uaParser from 'ua-parser-js'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
+app.get('*', (req, res) => {
+  const parsedUA = uaParser(req.headers['user-agent'])
+  const software = `${parsedUA.os.name} ${parsedUA.os.version}`
+
+  res.json({
+    "ipaddress": req.ip,
+    "language": req.acceptsLanguages()[0],
+    "software": software
+  })
 })
 
 const port = 3000
